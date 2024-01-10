6a5a6df6623727a4f485a3761c0411c2fbf812fe
Marcelo Salvatori <marcelo1302a@gmail.com>
2024-01-10 08:57:20 -0300

    Inicio do projeto Sabor Express

---
+++ app.py
@@ -0,0 +1,9 @@
+print('Sabor Express\n')
+
+print('1. Cadastrar Restaurante')
+print('2. Listar Restaurante')
+print('3. Ativar Restaurante')
+print('4. Sair')
+
+opcao_escolhida = input('\nEscolha uma opção: ')
+print(f'A opção escolhida foi: {opcao_escolhida}.')
