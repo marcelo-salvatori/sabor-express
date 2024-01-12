42653b95f17b795f537de7b346258646f45e39ba
Marcelo Salvatori <marcelo1302a@gmail.com>
2024-01-12 15:44:14 -0300

    doc: Adicionar docstrings descrevendo as funções

--- app.py
+++ app.py
@@ -1,22 +1,21 @@
-"""
-feat: uma nova feature (recurso) que você está adicionando a uma aplicação específica ;
-fix: a resolução de um bug;
-style: recurso e atualizações relacionadas à estilização;
-refactor: refatoração de uma seção específica da base de código;
-test: tudo o que for relacionado a testes;
-docs: tudo o que for relacionado à documentação;
-chore: manutenção regular do código. (Você também pode usar emojis para representar os tipos de commit).
-"""
-
 import os
 
 restaurantes = []
 
 def menu_principal():
+    '''
+    Retorna o usuário o menu principal
+    '''
     input('\nAperte enter para voltar ao menu principal')
     main()   
 
 def exibir_subtitulo(texto):
+    '''
+    Limpa a tela e exibe o subtítulo que cada submenu do programa.
+    
+    - Argumentos:
+        texto: String
+    '''
     os.system('cls')
     linha = '=' * (len(texto))
     print(linha)
@@ -25,6 +24,14 @@ def exibir_subtitulo(texto):
     print()
 
 def cadastrar_restaurante():
+    '''
+    Recebe inputs do usuário para cadastra um novo restaurante e adiciona os dados num dicionário
+
+    - Input:
+        Nome do restaurante: String
+        Categoria do restaurante: String
+    
+    '''
     exibir_subtitulo('Cadastro de Restaurantes')
     nome_restaurante = input('Digite o nome do restaurante que você deseja cadastrar: ')
     categoria_do_restaurante = input('Digite a categoria a qual esse restaurante pertence: ')
@@ -37,6 +44,9 @@ def cadastrar_restaurante():
     menu_principal()
 
 def listar_restaurante():
+    '''
+    Mostra um sub-menu com uma tabela contendo informações de todos os restaurantes cadastrados
+    '''
     exibir_subtitulo('Lista de restaurantes cadastradados: ')
 
     cabeçalho = f'{'Nome'.ljust(20)}|{'Categoria'.ljust(20)}|Status'
@@ -55,6 +65,12 @@ def listar_restaurante():
     menu_principal()
 
 def alternar_status_restaurante():
+    '''
+    Alterna o status entre \'ativo\' e \'inativo\' de algum restaurante cadastrado
+
+    -Input:
+        Nome do restaurante: String
+    '''
     exibir_subtitulo('Alternar Status do restaurante')
 
     nome_restaurante = input('Digite o nome do restaurante que você deseja alternar o status: ')
@@ -87,16 +103,26 @@ def exibir_nome_programa():
 """)
 
 def exibir_opcoes():
+    '''
+    Exibe um menu de opções que o usuário pode selecionar para exercutar as funções do programa
+    '''
     print('1. Cadastrar Restaurante')
     print('2. Listar Restaurante')
     print('3. Alternar status do Restaurante')
     print('4. Sair')
 
 def opcao_invalida():
+    '''
+    Função que faz o usuário retornar ao menu principal, caso escolha uma opção inválida
+    '''
     print('Opção inválida!\n')
     menu_principal()
 
 def escolher_opcao():
+    '''
+    Recebe o input de usuário e executa a função de acordo com a opção selecionada.
+    Caso o usuário digita algo que não seja um número inteiro, ou que não seja um número de 1 a 4, ele retornará ao menu principal
+    '''
     try:
         opcao_escolhida = int(input('\nEscolha uma opção: '))
 
@@ -122,43 +148,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-"""
-Beautiful is better than ugly.\
-
-Explicit is better than implicit.\
-
-Simple is better than complex.\
-
-Complex is better than complicated.\
-
-Flat is better than nested.\
-
-Sparse is better than dense.\
-
-Readability counts.\
-
-Special cases aren't special enough to break the rules.\
-
-Although practicality beats purity.\
-
-Errors should never pass silently.\
-
-Unless explicitly silenced.\
-
-In the face of ambiguity, refuse the temptation to guess.\
-
-There should be one-- and preferably only one --obvious way to do it.\
-
-Although that way may not be obvious at first unless you're Dutch.\
-
-Now is better than never.\
-
-Although never is often better than right now.\
-
-If the implementation is hard to explain, it's a bad idea.\
-
-If the implementation is easy to explain, it may be a good idea.\
-
-Namespaces are one honking great idea -- let's do more of those!\
-"""
